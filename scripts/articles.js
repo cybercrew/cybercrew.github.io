c6dcdd695a03bd2778ed965f214777a8a09d6d90
meesam4687 <meesam4687@gmail.com>
2024-04-29 15:35:41 +0530

    Show articles again

---
+++ scripts/articles.js
@@ -0,0 +1,19 @@
+fetch("https://backend-ddu1.onrender.com/articles")
+
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById('articles').innerHTML = "";
+        for (let art of data) {
+            let article = document.createElement("div");
+            article.setAttribute('onclick', `window.location.assign(".")`);
+            article.id = "art";
+            article.className = "art clickable";
+            article.innerHTML = `
+            <div class="artImage" id="artimg ${art.title.split(' ').join('_')}">
+            </div>
+            <p class="artTitle">${art.title}</p>
+            `;
+            document.getElementById("articles").appendChild(article);
+            document.getElementById("artimg " + art.title.split(' ').join('_')).style.backgroundImage = `url("${art.imgUrl}")`;
+        }
+    })
