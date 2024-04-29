362d7fa122c844f92dc2a38b318db4ab059558a9
meesam4687 <meesam4687@gmail.com>
2024-04-29 19:00:36 +0530

    feat: Added article page

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,19 +1,21 @@
-fetch("https://backend-ddu1.onrender.com/articles")
+function articles() {
+    fetch("https://backend-ddu1.onrender.com/articles")
 
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById('articles').innerHTML = "";
-        for (let art of data) {
-            let article = document.createElement("div");
-            article.setAttribute('onclick', `window.location.assign(".")`);
-            article.id = "art";
-            article.className = "art clickable";
-            article.innerHTML = `
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById('articles').innerHTML = "";
+            for (let art of data) {
+                let article = document.createElement("div");
+                article.setAttribute('onclick', `window.location.assign("../article.html?title=${art.title.split(' ').join('_')}")`);
+                article.id = "art";
+                article.className = "art clickable";
+                article.innerHTML = `
             <div class="artImage" id="artimg ${art.title.split(' ').join('_')}">
             </div>
             <p class="artTitle">${art.title}</p>
             `;
-            document.getElementById("articles").appendChild(article);
-            document.getElementById("artimg " + art.title.split(' ').join('_')).style.backgroundImage = `url("${art.imgUrl}")`;
-        }
-    })
+                document.getElementById("articles").appendChild(article);
+                document.getElementById("artimg " + art.title.split(' ').join('_')).style.backgroundImage = `url("${art.imgUrl}")`;
+            }
+        })
+}
