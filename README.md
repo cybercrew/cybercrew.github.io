e50057ed0a535bc1b81d65a43ec8c3fb914e3912
Saad Aamir <saad6101@students.dpssgr.com>
2025-04-12 18:27:19 +0530

    Initial commit
    A

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CyberCrew Website
 Website for CyberCrew DPS
 [View Here](https://cybercrew-dps.vercel.app)
+<<<<<<< HEAD
+=======
+# cybercrew
+>>>>>>> a175085 (Initial commit)
--- scripts/articles.js
+++ scripts/articles.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function articles() {
     fetch("https://website-backend-y7wt.onrender.com/articles")
 
@@ -24,3 +25,31 @@ function articles() {
             }
         })
 }
+=======
+// function articles() {
+//     fetch("https://website-backend-y7wt.onrender.com/articles")
+
+//         .then(res => res.json())
+//         .then(data => {
+//             document.getElementById('articles').innerHTML = "";
+//             for (let art of data) {
+//                 let article = document.createElement("div");
+//                 if (art.tags.includes("youtube")) {
+//                     article.setAttribute('onclick', `window.location.assign("${art.ytUrl}")`);
+//                 }
+//                 else {
+//                     article.setAttribute('onclick', `window.location.assign("../article?title=${art.title.split(' ').join('_')}")`);
+//                 }
+//                 article.id = "art";
+//                 article.className = "art clickable";
+//                 article.innerHTML = `
+//             <div class="artImage" id="artimg ${art.title.split(' ').join('_')}">
+//             </div>
+//             <p class="artTitle">${art.title}</p>
+//             `;
+//                 document.getElementById("articles").appendChild(article);
+//                 document.getElementById("artimg " + art.title.split(' ').join('_')).style.backgroundImage = `url("${art.imgUrl}")`;
+//             }
+//         })
+// }
+>>>>>>> a175085 (Initial commit)
