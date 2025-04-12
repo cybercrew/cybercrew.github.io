9a189e49c6fa941946953fd83f8f84234d6afc31
Saad Aamir <saad6101@students.dpssgr.com>
2025-04-12 18:36:22 +0530

    Chages

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-function articles() {
-    fetch("https://website-backend-y7wt.onrender.com/articles")
-
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById('articles').innerHTML = "";
-            for (let art of data) {
-                let article = document.createElement("div");
-                if (art.tags.includes("youtube")) {
-                    article.setAttribute('onclick', `window.location.assign("${art.ytUrl}")`);
-                }
-                else {
-                    article.setAttribute('onclick', `window.location.assign("../article?title=${art.title.split(' ').join('_')}")`);
-                }
-                article.id = "art";
-                article.className = "art clickable";
-                article.innerHTML = `
-            <div class="artImage" id="artimg ${art.title.split(' ').join('_')}">
-            </div>
-            <p class="artTitle">${art.title}</p>
-            `;
-                document.getElementById("articles").appendChild(article);
-                document.getElementById("artimg " + art.title.split(' ').join('_')).style.backgroundImage = `url("${art.imgUrl}")`;
-            }
-        })
-}
-=======
 // function articles() {
 //     fetch("https://website-backend-y7wt.onrender.com/articles")
 
@@ -52,4 +24,3 @@ function articles() {
 //             }
 //         })
 // }
->>>>>>> a175085 (Initial commit)
